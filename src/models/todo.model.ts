0c86d3168399042b13e831ed63797f4afadbaa7f
Raniel <raniel25sobrinho@gmail.com>
2020-12-24 13:05:58 -0300

    Refactoring the TS file

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -1,11 +1,9 @@
 export class Todo {
-  /**
-   *Quando estanciar o todo no app.component aparecerá
-   *quais são os requisitos da classe
-   */
+// Quando estanciar o todo no app.component aparecerá
+// quais são os requisitos da classe
   constructor(
-    public id: Number,
-    public title: String,
-    public done: Boolean,
+    public id: number,
+    public title: string,
+    public done: boolean,
   ) {  }
 }
