28fe4b47bf6345c4a239d760e6a3e27fdf2c89a4
Raniel <raniel25sobrinho@gmail.com>
2020-12-23 23:14:37 -0300

    Initial app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = []; // pegando a classe todo.model.ts
+  public title: String = "Minhas tarefas:"
+
+  constructor() {
+    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'Ir ao supermercado', false));
+    this.todos.push(new Todo(3, 'Cortar o cabelo', true));
+
+  }
+
 }
