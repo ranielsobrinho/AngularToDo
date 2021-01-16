ba31d366a283e2a0d939227b209b4ca109830360
Raniel <raniel25sobrinho@gmail.com>
2021-01-16 13:41:24 -0300

    Express server to load the app for heroku deploy

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();  
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/dist/todo'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/dist/todo/index.html');
+});
+
+app.listen(PORT, () => {
+    console.log("Servidor iniciado na porta " + PORT);
+});
