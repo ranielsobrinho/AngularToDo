778cf3a78c7e3096d32078e2d465226df389a134
Raniel <raniel25sobrinho@gmail.com>
2021-04-24 16:05:38 -0300

    Delete the methods and added in the service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Todo } from 'src/models/todo.model';
 import { faPlusCircle, faTimes, faCheckCircle, faWindowClose, faUndo } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
@@ -9,7 +8,6 @@ import { faPlusCircle, faTimes, faCheckCircle, faWindowClose, faUndo } from '@fo
 })
 export class AppComponent {
   public mode = 'list';
-  public todos: Todo[] = [];
   public title = 'Lista de tarefas';
   public form: FormGroup;
 
@@ -28,55 +26,8 @@ export class AppComponent {
         Validators.required
       ])]
     });
-
-    this.load();
   }
 
-  add(): void {
-    const title = this.form.controls.title.value;
-    const id = this.todos.length + 1;
-    this.todos.push(new Todo(id, title, false));
-    this.save();
-    this.clear();
-  }
-
-  clear(): void {
-    this.form.reset();
-  }
-
-  remove(todo: Todo): void{
-    const index = this.todos.indexOf(todo);
-    if (index !== -1){
-      this.todos.splice(index, 1); // Get the index e then delete
-      this.save();
-    }
-  }
-
-  markAsDone(todo: Todo): void{
-    todo.done = true;
-    this.save();
-  }
-
-  markAsUndone(todo: Todo): void{
-    todo.done = false;
-    this.save();
-  }
-
-  save(): void{
-    const data = JSON.stringify(this.todos);
-    localStorage.setItem('todos', data);
-    this.mode = 'list';
-    // You can use removeItem to remove an item and clear to remove everything
-  }
-
-  load(): any{
-    const data = localStorage.getItem('todos');
-    if (data) {
-      this.todos = JSON.parse(data);
-    } else {
-      this.todos = [];
-    }
-  }
 
   changeMode(mode): any{
     this.mode = mode;
