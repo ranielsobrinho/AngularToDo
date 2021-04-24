ab8948c96cbcdd7463f7e1fe57b29ee3ae1e02f2
Raniel <raniel25sobrinho@gmail.com>
2021-04-24 16:06:21 -0300

    In the array we can acess the todo id with the index, so we don't need to pass it manually

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -2,7 +2,6 @@ export class Todo {
 // Quando estanciar o todo no app.component aparecerá
 // quais são os requisitos da classe
   constructor(
-    public id: number,
     public title: string,
     public done: boolean,
   ) {  }
