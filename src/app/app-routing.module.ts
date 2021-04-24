1ce1553244e1d522d12b5b3ff3592ea1c28f708f
Raniel <raniel25sobrinho@gmail.com>
2021-04-24 18:52:57 -0300

    Edited

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 
