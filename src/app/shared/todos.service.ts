a3330c411ff6f90430d2ba3af616dd07d8701483
Raniel <raniel25sobrinho@gmail.com>
2021-04-24 16:05:04 -0300

    Add methods in the todos service

--- src/app/shared/todos.service.ts
+++ src/app/shared/todos.service.ts
@@ -1,9 +1,54 @@
+import { identifierModuleUrl } from '@angular/compiler';
 import { Injectable } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodosService {
-
+  public todos: Todo[] = [];
   constructor() { }
+
+  add(title: string): any {
+    const newLenght = this.todos.push(new Todo(title, false));
+    const index = newLenght - 1;
+    this.save();
+    return index;
+  }
+
+  remove(id: number): void{
+      this.todos.splice(id, 1); // Get the index e then delete
+      this.save();
+  }
+
+  markAsDone(todo: Todo): void{
+    todo.done = true;
+    this.save();
+  }
+
+  update(id: number, title: string): any{
+    const todo = this.todos[id];
+    todo.title = title;
+    this.save();
+  }
+
+  markAsUndone(todo: Todo): void{
+    todo.done = false;
+    this.save();
+  }
+
+  save(): void{
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+    // You can use removeItem to remove an item and clear to remove everything
+  }
+
+  load(): any{
+    const data = localStorage.getItem('todos');
+    if (data) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
+  }
 }
