11a5f62c7b4187b26dfb2bfc70f747ecf3d9b283
Raniel <raniel25sobrinho@gmail.com>
2021-04-25 21:30:16 -0300

    Building the form and add a method that calls the service method to add a new todo in the localStorage

--- src/app/pages/todo-list/todo-list.component.ts
+++ src/app/pages/todo-list/todo-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TodosService } from 'src/app/shared/todos.service';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent {
+  public form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder,
+              private todosService: TodosService) {
+                this.form = this.fb.group({
+                  title: ['', Validators.compose([
+                    Validators.minLength(3),
+                    Validators.maxLength(90),
+                    Validators.required
+                  ])]
+                });
+              }
+  addTodo(): any{
+    const title = this.form.controls.title.value;
+    this.todosService.add(title);
+    this.form.reset();
   }
-
 }
