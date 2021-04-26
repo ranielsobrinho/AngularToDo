997e825508c7c78671f644101d3fb79c67647f89
Raniel <raniel25sobrinho@gmail.com>
2021-04-26 19:59:00 -0300

    Removed unused modules and add card module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,10 +4,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {MatDialogModule} from '@angular/material/dialog';
-import {MatDividerModule} from '@angular/material/divider';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
     imports: [
@@ -15,20 +15,20 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
         MatButtonModule,
         MatIconModule,
         MatDialogModule,
-        MatDividerModule,
         MatFormFieldModule,
         MatInputModule,
-        MatSnackBarModule
+        MatSnackBarModule,
+        MatCardModule
     ],
     exports: [
         MatToolbarModule,
         MatButtonModule,
         MatIconModule,
         MatDialogModule,
-        MatDividerModule,
         MatFormFieldModule,
         MatInputModule,
-        MatSnackBarModule
+        MatSnackBarModule,
+        MatCardModule
     ]
 })
 export class MaterialModule { }
