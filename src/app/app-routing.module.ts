30281e57f2f7025564497c35619c1e36014e3b0b
Raniel <raniel25sobrinho@gmail.com>
2021-05-06 23:26:48 -0300

    Add 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { TodoListComponent } from './pages/todo-list/todo-list.component';
 
 const routes: Routes = [
   {path: '', component: MainLayoutComponent, children: [
     {path: '', component: TodoListComponent}
-  ]}
+  ]},
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
