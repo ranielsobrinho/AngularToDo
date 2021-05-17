7413d5cb74025231e0e54b13c207580db2413668
Raniel <raniel25sobrinho@gmail.com>
2021-05-16 22:42:54 -0300

    Main functions and method to update the todo

--- src/app/pages/form-dialog/form-dialog.component.ts
+++ src/app/pages/form-dialog/form-dialog.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { TodosService } from 'src/app/shared/todos.service';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-form-dialog',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-dialog.component.css']
 })
 export class FormDialogComponent implements OnInit {
+  public form: FormGroup;
+  public todoId: number;
+  public todo: Todo;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+              private todosService: TodosService,
+              private route: ActivatedRoute,
+              private router: Router) {
+                this.form = this.fb.group({
+                  title: ['', Validators.compose([
+                    Validators.minLength(3),
+                    Validators.maxLength(90),
+                    Validators.required
+                  ])]
+                });
+               }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      if (params.id) {
+        this.todoId = params.id;
+        this.todo = this.todosService.get(this.todoId);
+        console.log(this.todo);
+      }
+    });
+  }
+
+  addTodo(): any{
+    const title = this.form.controls.title.value;
+    this.todosService.update(this.todoId, title);
+    this.router.navigateByUrl('/');
+    this.todosService.showMessage('Tarefa atualizada com sucesso');
   }
 
 }
