6fbca2fa20450b96d8c51855a906388e8c2b5a52
Raniel <raniel25sobrinho@gmail.com>
2021-05-16 22:41:28 -0300

    Add new route to update the todo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormDialogComponent } from './pages/form-dialog/form-dialog.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { TodoListComponent } from './pages/todo-list/todo-list.component';
 
 const routes: Routes = [
   {path: '', component: MainLayoutComponent, children: [
-    {path: '', component: TodoListComponent}
+    {path: '', component: TodoListComponent},
+    {path: ':id', component: FormDialogComponent}
   ]},
   {path: '**', component: NotFoundComponent}
 ];
