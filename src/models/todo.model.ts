c7c1fb42b01e14c84e2f0031b0b731ff44c25220
Raniel <raniel25sobrinho@gmail.com>
2021-07-08 23:10:03 -0300

    Add id to model

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -2,6 +2,7 @@ export class Todo {
 // Quando estanciar o todo no app.component aparecerá
 // quais são os requisitos da classe
   constructor(
+    public id: number,
     public title: string,
     public done: boolean,
   ) {  }
